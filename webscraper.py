3baffb5fc03d6cd42ba99ad76573b52d946032d5
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-09-21 11:11:11 -0700

    numbered books

--- webscraper.py
+++ webscraper.py
@@ -5,13 +5,11 @@ URL = "https://www.goodreads.com/shelf/show/philosophy"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+books = soup.findAll(class_="left")
 
-titles = soup.findAll(class_="bookTitle")
-authors = soup.findAll(class_="authorName")
-
-
-for title in titles:
-    print(title.text)
-
-for author in authors:
-    print(author.text)
+a = 1
+for book in books:
+    title = book.find(class_="bookTitle")
+    author = book.find(class_="authorName")
+    print (a, title.text + " by " + author.text + "\n")
+    a = a + 1
