21c336a5f8344066b5aafcfcfa05a75d56dd5576
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-10-03 16:11:47 -0700

    print book and author from top 50 books from each genre

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,26 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-URL = "https://www.goodreads.com/shelf/show/philosophy"
-page = requests.get(URL)
+URL = "https://www.goodreads.com/shelf/show"
+num = 1
 
+HOME = "https://www.goodreads.com"
+
+genres = []
+page = requests.get(HOME)
 soup = BeautifulSoup(page.content, "html.parser")
-books = soup.findAll(class_="left")
+gs = soup.findAll(class_="gr-hyperlink")
+for g in gs:
+    genres.append(g.text)
+
 
 a = 1
-for book in books:
-    title = book.find(class_="bookTitle")
-    author = book.find(class_="authorName")
-    print (a, title.text + " by " + author.text + "\n")
-    a = a + 1
+for genre in genres:
+    page = requests.get(URL + "/" + genre)
+    soup = BeautifulSoup(page.content, "html.parser")
+    books = soup.findAll(class_="left")
+    for book in books:
+        title = book.find(class_="bookTitle")
+        author = book.find(class_="authorName")
+        print (a, title.text + " by " + author.text + "\n")
+        a = a + 1   
