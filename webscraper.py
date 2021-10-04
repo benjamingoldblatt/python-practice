e95a6a92bfb6a2e2f523f34f4c90e8248fa18312
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-10-03 18:00:25 -0700

    created panda database w/ 5 columns for each book

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,14 +15,41 @@ gs = soup.findAll(class_="gr-hyperlink")
 for g in gs:
     genres.append(g.text)
 
-
+books = []
+data = {"title": [], "author": [], "rating": [], "reviews": [], "year": []}
 a = 1
 for genre in genres:
+    if (a > 200):
+        break
     page = requests.get(URL + "/" + genre)
     soup = BeautifulSoup(page.content, "html.parser")
     books = soup.findAll(class_="left")
     for book in books:
         title = book.find(class_="bookTitle")
         author = book.find(class_="authorName")
-        print (a, title.text + " by " + author.text + "\n")
+        rating = book.find(class_="greyText smallText")
+        ## avg rating 4.45 —\n                26,684 ratings  —\n                published 2011
+        rating = rating.text.strip()
+
+        punc = '.,' 
+        for ele in rating:
+            if ele in punc:
+                rating = rating.replace(ele, "")
+
+        numbers = []
+        for word in rating.split():
+            if word.isdigit():
+                numbers.append(int(word))
+
+        numbers[0] = numbers[0]/100
+        if (len(numbers) == 3):
+            data["title"].append(title.text)
+            data["author"].append(author.text)
+            data["rating"].append(numbers[0])
+            data["reviews"].append(numbers[1])
+            data["year"].append(numbers[2])
         a = a + 1   
+
+df = pd.DataFrame(data)
+
+print(df)
