56743e0b439442b44bed2ad5b9071ac6f57afaad
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-10-06 16:57:52 -0700

    made csv file with links of top 3000 books

--- bestbooks.py
+++ bestbooks.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import grequests
 from bs4 import BeautifulSoup
 import requests
 
@@ -8,32 +9,39 @@ BESTBOOKS = "https://www.goodreads.com/list/show/264.Books_That_Everyone_Should_
 BOOK = "https://www.goodreads.com"
 
 bookurls = []
+data = {"genre":[], "title": [], "authors": [], "rating": [], "reviews": [], "year": [], "description": []}
+
 a = 1
-while a < 3:
+while a < 2:
     page = requests.get(BESTBOOKS + str(a))
     soup = BeautifulSoup(page.content, "html.parser")
     books = soup.findAll("a", class_="bookTitle", href=True)
     for book in books:
         url = book["href"]
-        bookurls.append(url)
+        bookurls.append("https://www.goodreads.com" + url)
     a = a+1
-print(bookurls)
+df = pd.DataFrame(bookurls)
+df.to_csv('bestbooks.csv', index=False)
 
-data = {"genre":[], "title": [], "authors": [], "rating": [], "reviews": [], "year": [], "description": []}
+reqs = (grequests.get(link) for link in bookurls)
+resp = grequests.map(reqs, size=40)
 
-for book in bookurls:
-    page = requests.get(BOOK + book)
-    soup = BeautifulSoup(page.content, "html.parser")
+for r in resp:
+    soup = BeautifulSoup(r.content, "html.parser")
     title = soup.find(id="bookTitle")
-    data["title"].append(title.text)
+    names = []
     authors = soup.findAll(class_="authorName")
     for auth in authors:
-        names = []
         names.append(auth.text)
-        data["authors"].append(names)
+    print(names)
     genre = soup.find(class_="actionLinkLite bookPageGenreLink")
-    data["genre"].append(genre)
-    rating = soup.find(itemprop="ratingValue")
-    data["rating"] = rating.text
+    rating = soup.find('span',attrs={"itemprop":"ratingValue"})
+    if title and genre and rating and authors:
+        data["authors"].append(names)
+        data["title"].append(title.text.strip())
+        data["genre"].append(genre.text)
+        data["rating"].append(rating.text.strip())
+    print(data)
+print(len(data["rating"]))
 
-print(data)
+
